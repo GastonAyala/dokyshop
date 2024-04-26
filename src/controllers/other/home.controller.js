f9af2d1d48399eb1f83ba7d9dd3cb7b4d21b2ffa
GastonAyala <gastonayalagimenez@gmail.com>
2024-04-26 03:58:03 -0300

    arregle relación entre modelos product e imagesecondary, cambie el nombre del modelo imagesecondary y tabla, migracion y seeder, agrego detalle de producto refactorizado a db

--- src/controllers/other/home.controller.js
+++ src/controllers/other/home.controller.js
@@ -1,11 +1,22 @@
 const db = require('../../database/models');
 
 module.exports = (req, res) => {
-    db.product.findAll()
+    db.otherImage.findAll({
+        where: {
+            viewId: 1
+        }
+    })
+    .then((images) => {
+        db.product.findAll({
+            limit: 4
+        })
     .then(products => {
-        res.render("other/home", {products})
+        res.render("other/home", { products, images })
     })
     .catch(err => {
         res.send(err.message)
     })
+
+    })
+    
 }
