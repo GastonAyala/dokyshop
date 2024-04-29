31eac974db61d453dcbe29a92be14c21d61b3bf2
Adriana <103216065+facundocaro1826@users.noreply.github.com>
2024-04-29 00:30:57 -0300

    rendirice la vista crear producto, cree la funcionalidad de crear producto, modifique la migracion de crear producto donde la propiedad type acepte un string de 45 caracteres

--- src/controllers/admin/store.controller.js
+++ src/controllers/admin/store.controller.js
@@ -1,48 +1,42 @@
 const path = require('path');
 const fs = require('fs');
 
-const { loadData, saveData } = require('../../data');
+const db = require("../../database/models")
+
 
 module.exports = (req, res) => {
+  
+    const {title, category, subcategory,description, price, sale, quantity, color, available, imagePrimary, imagesSecondary} = req.body;
    
-    const {title, category, description, price, sale, quantity, color} = req.body;
-       
-    let products = loadData();
-        
-    const newId = products[products.length - 1].id + 1;
-
-    let newImages = [];
-    if(req.files.imagesSecondary?.length){
-        newImages = req.files.imagesSecondary?.map((img) => img.filename)
-    } 
-    
-    const newProduct = {
-        id: newId,
+  //return res.send(req.body)
+    db.product.create({
         title: title.trim(),
-        category: category?.trim(), 
+        categoryId: +category?.trim(),
+        subcategoryId: +subcategory?.trim(),  
         description: description.trim(),             // trim() quita los espacios(delante, entre y final de una palabra)
         price: +price,
         sale: +sale,
         quantity: +quantity,
-        color: color.trim(),
-        imagePrimary: req.files.imagePrimary?.length ? req.files.imagePrimary[0]?.filename : ["no-image.png"],
-        imagesSecondary: newImages.length ? newImages : ["no-image.png"]    
-        };
-
-        products = [...products, newProduct];
-
-        products = JSON.stringify(products, null, 3);
-        const pathProducts = path.join(__dirname, "../../data/product.json");
-        fs.writeFileSync(pathProducts, products, "utf-8")
-
-    
-        res.redirect(`/admin/productos`)},
-
-        (err, contentView) => {
-        err && res.send(err.message)
-        res.render("./partials/dashboard", {
-        contentView
-      
+        colorId: null,
+        available: available === "on",
+        imagePrincipal: req.files.imagePrimary?.length
+         ? req.files.imagePrimary[0]?.filename
+          : "no-image.png"
     })
-   
-};
+    .then((product)=>{
+    let newImages = [];  
+    if(req.files.imagesSecondary?.length){
+            newImages = req.files.imagesSecondary?.map((img) => {
+                return {
+                    file: img.filename,
+                    productId : product.id
+                }
+            })
+         } 
+    db.imagesecundary.bulkCreate(newImages)
+    .then(()=>{
+       
+        return res.redirect(`/admin/productos`)
+    })    
+ })
+   }
--- src/database/migrations/20240423042616-create-imagesecundary.js
+++ src/database/migrations/20240423042616-create-imagesecundary.js
@@ -10,7 +10,7 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       file: {
-        type: Sequelize.STRING(35)
+        type: Sequelize.STRING(45)
       },
       productId: {
         type: Sequelize.INTEGER,
