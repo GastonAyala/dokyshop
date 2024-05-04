837ab3c2a3df66abcb55a272086f1960e3fa42e1
GastonAyala <gastonayalagimenez@gmail.com>
2024-05-03 23:53:16 -0300

    correcciones  en el controlador de crear productos y vista admin listado de productos

--- src/controllers/admin/store.controller.js
+++ src/controllers/admin/store.controller.js
@@ -1,42 +1,38 @@
-const path = require('path');
-const fs = require('fs');
-
 const db = require("../../database/models")
 
-
 module.exports = (req, res) => {
-  
-    const {title, category, subcategory,description, price, sale, quantity, color, available, imagePrimary, imagesSecondary} = req.body;
-   
-  //return res.send(req.body)
+
+    const { title, category, subcategory, description, price, sale, quantity, color, available } = req.body;
+
     db.product.create({
         title: title.trim(),
         categoryId: +category?.trim(),
-        subcategoryId: +subcategory?.trim(),  
-        description: description.trim(),             // trim() quita los espacios(delante, entre y final de una palabra)
+        subcategoryId: +subcategory?.trim(),
+        description: description.trim(),
         price: +price,
         sale: +sale,
         quantity: +quantity,
         colorId: null,
         available: available === "on",
         imagePrincipal: req.files.imagePrimary?.length
-         ? req.files.imagePrimary[0]?.filename
-          : "no-image.png"
+            ? req.files.imagePrimary[0]?.filename
+            : "no-image.png"
     })
-    .then((product)=>{
-    let newImages = [];  
-    if(req.files.imagesSecondary?.length){
+    .then((product) => {
+        let newImages = [];
+        if (req.files.imagesSecondary?.length) {
             newImages = req.files.imagesSecondary?.map((img) => {
                 return {
                     file: img.filename,
-                    productId : product.id
+                    productId: product.id
                 }
             })
-         } 
-    db.imagesecundary.bulkCreate(newImages)
-    .then(()=>{
-       
-        return res.redirect(`/admin/productos`)
-    })    
- })
-   }
+        } else {
+            newImages = [{ file: "no-image.png", productId: product.id }]
+        }
+        db.imagesecondary.bulkCreate(newImages)
+        .then(() => {
+            return res.redirect(`/admin/productos`)
+        })  
+    })
+};
