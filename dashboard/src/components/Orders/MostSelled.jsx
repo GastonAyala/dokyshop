53d249fc5bde01790dc64dcb2ffe57a46993bba8
GastonAyala <gastonayalagimenez@gmail.com>
2024-06-24 03:20:19 -0300

    correciones generales y agrego panel del último usuarios registrado

--- dashboard/src/components/Orders/MostSelled.jsx
+++ dashboard/src/components/Orders/MostSelled.jsx
@@ -4,42 +4,29 @@ import { Alert } from '../reusable/Alert';
 
 
 export const MostSelled = () => {
-    const [orders, setOrders] = useState([]);
     const [mostSaled, setMostSaled] = useState([]);
     const [loading, setLoading] = useState(true);
     const [stateError, setStateError] = useState("");
 
     useEffect(() => {
-        const getCompletedOrders = async () => {
-            const endpoint = 'http://localhost:3030/api/order/completed';
+        const getMostSelled = async () => {
+            const endpoint = `http://localhost:3030/api/query?q=SELECT p.id, p.title, SUM(op.quantity) "totalSales" FROM dokyshop.orders o  INNER JOIN dokyshop.orderproducts op  ON o.id = op.orderId  INNER JOIN dokyshop.products p  ON op.productId = p.id WHERE o.state = "completed" GROUP BY p.id, p.title ORDER BY SUM(op.quantity) DESC LIMIT 5;`
             try {
                 const { ok, data = [], msg = null } = await fetch(endpoint).then(res => res.json());
-
                 if (!ok) throw new Error(msg);
 
-                ok && setOrders(data);
-
+                ok && setMostSaled(data);
                 setTimeout(() => setLoading(false), 1300);
-                
             } catch (error) {
                 setStateError(error.message)
             }
         };
 
-        getCompletedOrders();
-    }, [])
-
-    useEffect(() => {
-        let products = [];
-        orders.map(o => {
-            products = [...products, ...o.products];
-        })
-        setMostSaled(products);
-
-    }, [orders]);
+        getMostSelled();
+    }, []);
 
     return (
-        <div className={`text-center`}>
+        <div className={`col-lg-6 text-center`}>
             {!loading ? <div className="card shadow mb-4">
                 <div className="card-header py-3">
                     <h5 className="m-0 font-weight-bold text-gray-800 text-left" style={{ fontSize: "calc(0.94756rem + 0.3vw)" }}>
@@ -51,10 +38,11 @@ export const MostSelled = () => {
                         {mostSaled.map((p, i) => {
                             return <div key={i} className="col-lg-4 mb-4 p-2">
                                 <div className="card bg-dark text-white shadow">
-                                    <div className="card-body">
+                                    <div className="card-body d-flex flex-wrap justify-content-center">
                                         <a href={`http://localhost:3030/productos/detalle/${p.id}`}>
-                                            <span className='w-100 mb-1' style={{color: "white" }}>{p.title.substring(0, 35)}{p.title.length > 35 ? "..." : null}</span>
+                                            <span className='w-100 mb-1' style={{ color: "white" }}>{p.title.substring(0, 35)}{p.title.length > 35 ? "..." : null}</span>
                                         </a>
+                                            <span className='w-100 m-auto'>{p.totalSales}</span>
                                     </div>
                                 </div>
                             </div>
