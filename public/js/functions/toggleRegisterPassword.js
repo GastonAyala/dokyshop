bc1f6dc0c2b1d01d1ef062bf9b4bb4ee8aef96be
rika-tula <ricazabala04@gmail.com>
2024-07-13 19:17:50 -0300

    agregue funcionalidad para ver la contraseña en los formularios de iniciar sesión y registrarse

---
+++ public/js/functions/toggleRegisterPassword.js
@@ -0,0 +1,17 @@
+window.addEventListener('load', () => {
+    const inputPass2 = document.querySelector('#contrasenia')
+    const btnEye = document.querySelector('#btn-eye')
+    const iconEye = document.querySelector('#icon-eye')
+
+    btnEye.addEventListener('click', (e) => {
+        const isActive = iconEye.classList.contains('fa-eye-slash')
+        if (isActive) {
+            inputPass2.type = 'password';
+        } else {
+            inputPass2.type = 'text';
+        }
+        iconEye.classList.toggle('fa-eye');
+        iconEye.classList.toggle('fa-eye-slash');
+    })
+
+})
