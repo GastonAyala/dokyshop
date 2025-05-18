c4949cd681a920144f455dd895b131f507bc4eb4
GastonAyala <gastonayalagimenez@gmail.com>
2025-05-18 02:25:24 -0300

    Add server and proxy configuration in vite.config.js

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path';
 import { fileURLToPath } from 'url';
+const { VITE_API_HOST } = import.meta.env
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -12,5 +13,15 @@ export default defineConfig({
   build: {
     outDir: path.resolve(__dirname, '../public/dashboard'),
     emptyOutDir: true
-  }
+  },
+  server: {
+    host: true,
+    proxy: {
+      '/api': {
+        target: VITE_API_HOST,
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }}
 })
